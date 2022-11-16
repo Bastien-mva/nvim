cc8dd2a7bf4378f2e60b5a00a2089f8587dc6574
bastien-mva <bastien.batardiere@gmail.com>
2022-11-16 15:58:57 +0100

    Most important file. in cmp, there are the configuration for the autocompletion files. In init.lua there are the main files required, such as packer, cmp and set. in set, there are all the files the setting. in packer, there are all the plugins required. You need to set under the next use statement : use 'repo/name'. Then :so %, and then :PackerSync. it will download all the github files. Maybe you need to close and reopen the packer.lua file to make it work.

---
+++ lua/bastien/packer.lua
@@ -0,0 +1,50 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'ThePrimeagen/vim-be-good'
+-- to see the branch name in the ligheline
+  use 'itchyny/vim-gitbranch'
+-- -- dark scheme 
+  use 'tomasiser/vim-code-dark'
+-- -- lightline 
+  use 'itchyny/lightline.vim'
+-- -- spawn a terminal 
+  use 'kassio/neoterm' 
+-- -- maximize the splits 
+  use 'szw/vim-maximizer'
+-- -- easily move across splits 
+  use 'christoomey/vim-tmux-navigator'
+-- -- Comment code 
+  use 'tpope/vim-commentary'
+-- -- format  code 
+  use 'sbdchd/neoformat' 
+-- -- pairs brackets 
+  use 'jiangmiao/auto-pairs'
+-- auto completion 
+  use 'hrsh7th/nvim-cmp'
+  -- plugins for autocompletion 
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'hrsh7th/cmp-nvim-lsp'
+end)
+-- -- auto completion 
+-- -- use 'nvim-lua/completion-nvim'
+-- -- required plugin for the auto-completion 
+-- -- use 'neovim/nvim-lspconfig'
+-- use 'hrsh6th/cmp-nvim-lsp'
+-- use 'hrsh6th/cmp-buffer'
+--use 'hrsh6th/cmp-path'
+-- use 'hrsh6th/cmp-cmdline'
+
+-- end)
---
+++ lua/bastien/set.lua
@@ -0,0 +1,24 @@
+vim.opt.guicursor = ""
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4 
+vim.opt.shiftwidth = 4 
+vim.opt.expandtab = true 
+vim.opt.hlsearch = false 
+vim.opt.incsearch = true 
+
+vim.opt.smartindent = true 
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 7
+
+-- Set statusbar
+vim.g.lightline = {
+	    colorscheme = 'codedark',
+	      active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
+	        component_function = { gitbranch = 'gitbranch#name'},
+	}
