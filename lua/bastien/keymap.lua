ed105833709bbb0b910b458038b0d74a0d849e7f
bastien-mva <bastien.batardiere@gmail.com>
2022-11-17 13:51:13 +0100

    added the keymaps to change windows.

--- lua/bastien/keymap.lua
+++ lua/bastien/keymap.lua
@@ -1,1 +1,18 @@
 vim.cmd[[autocmd FileType python nnoremap <buffer> <F9> :update<bar>!python %<CR>]]
+
+vim.g.mapleader = " "
+
+
+function map(mode, lhs, rhs, opts)
+        local options = { noremap = true }
+            if opts then
+                        options = vim.tbl_extend("force", options, opts)
+                            end
+                                vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+                            end
+
+
+map("n","<leader>m", ":MaximizerToggle!<CR>")
+map("n", "<c-q>", ":Ttoggle<CR>")
+map("i", "<c-q>", "<Esc>:Ttoggle<CR>")
+-- map("t", "<c-q>", "<c-\><c-n>:Ttoggle<CR>")
