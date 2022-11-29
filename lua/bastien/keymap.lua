b6fc68187851dd3c3ff23e9c344db452c9966949
bastien-mva <bastien.batardiere@gmail.com>
2022-11-29 23:23:09 +0100

    Change Ctrl j to leader j etc for tmux.

--- lua/bastien/keymap.lua
+++ lua/bastien/keymap.lua
@@ -1,5 +1,5 @@
-vim.cmd[[autocmd FileType python nnoremap <buffer> <F9> :w \| Dispatch python  % <CR>]]
-vim.cmd[[autocmd FileType python inoremap <buffer> <F9> :w \| Dispatch python  % <CR>]]
+vim.cmd[[autocmd FileType python nnoremap <buffer> <F8> :w \| Dispatch python  % <CR>]]
+vim.cmd[[autocmd FileType python inoremap <buffer> <F8> :w \| Dispatch python  % <CR>]]
 
 vim.g.mapleader = " "
 
@@ -12,7 +12,7 @@ function map(mode, lhs, rhs, opts)
                                 vim.api.nvim_set_keymap(mode, lhs, rhs, options)
                             end
 
-map("n","<leader>m", ":MaximizerToggle!<CR>")
+map("n","<leader>f", ":MaximizerToggle!<CR>")
 -- kassio/neoterm 
 map("n", "<c-q>", ":vs  | :Ttoggle<CR>")
 map("i", "<c-q>", "<Esc>| :vs | :Ttoggle<CR>")
@@ -25,9 +25,9 @@ map("n", "<leader><space>", ":GFiles<CR>")
 map("n", "gd", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>")
 
 vim.g.tmux_navigator_no_mappings = 1
-vim.cmd[[noremap <silent> <C-h> :w<CR>\|:<C-U>TmuxNavigateLeft<cr>]]
-vim.cmd[[noremap <silent> <C-j> :w<CR>\|:<C-U>TmuxNavigateDown<cr>]]
-vim.cmd[[noremap <silent> <C-k> :w<CR>\|:<C-U>TmuxNavigateUp<cr>]]
-vim.cmd[[noremap <silent> <C-l> :w<CR>\|:<C-U>TmuxNavigateRight<cr>]]
-vim.cmd[[noremap <silent> <C-,> :w<CR>\|:<C-U>TmuxNavigatePrevious<cr>]]
+vim.cmd[[noremap <silent> <leader>h :w<CR>\|:<C-U>TmuxNavigateLeft<cr>]]
+vim.cmd[[noremap <silent> <leader>j :w<CR>\|:<C-U>TmuxNavigateDown<cr>]]
+vim.cmd[[noremap <silent> <leader>k :w<CR>\|:<C-U>TmuxNavigateUp<cr>]]
+vim.cmd[[noremap <silent> <leader>l :w<CR>\|:<C-U>TmuxNavigateRight<cr>]]
+vim.cmd[[noremap <silent> <leader>, <CR>\|:<C-U>TmuxNavigatePrevious<cr>]]
 
