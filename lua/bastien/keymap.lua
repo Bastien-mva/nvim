3569912215e86a021f82bbdc25a8dab535749a9a
bastien-mva <bastien.batardiere@gmail.com>
2022-12-11 20:48:40 +0100

    command to run python files.

--- lua/bastien/keymap.lua
+++ lua/bastien/keymap.lua
@@ -1,5 +1,5 @@
 vim.cmd[[autocmd FileType python nnoremap <buffer> <F8> :w \| Dispatch python  % <CR>]]
-vim.cmd[[autocmd FileType python inoremap <buffer> <F8> :w \| Dispatch python  % <CR>]]
+vim.cmd[[autocmd FileType r nnoremap <buffer> <F8> :w \| Dispatch r  % <CR>]]
 
 vim.g.mapleader = " "
 
