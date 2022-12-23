27537c284f562d7f3d0abbda2ca7ce3466028b68
bastien-mva <bastien.batardiere@gmail.com>
2022-12-23 11:40:47 +0100

    ale fixers for python latex (and r ?).

---
+++ ftplugin/ale.lua
@@ -0,0 +1,4 @@
+vim.cmd[[let g:ale_linters = {'python': ['flake8']}]]
+vim.cmd[[let g:ale_fixers = {'python': ['black']}]]
+vim.cmd[[let g:ale_fixers = {'latex': ['latexindent']}]]
+-- vim.g.ale_linters = ['flake8'] 
