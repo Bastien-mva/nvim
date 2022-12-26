0a85e1d02b9083a284397c693c4c88e6f09b0ed4
bastien-mva <bastien.batardiere@gmail.com>
2022-12-26 11:17:49 +0100

    Added the remap for text object $. Added  remaps for copy pasting using clipboard from The primagean. Added a keymap to exit insert mode

--- lua/bastien/keymap.lua
+++ lua/bastien/keymap.lua
@@ -1,8 +1,11 @@
+-- vim.keymap.set("n", "<F2>", "x $ p")
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- deoplete auto completion for latex 
--- vim.cmd[[let g:deoplete#enable_at_startup = 1]]
+vim.cmd[[xnoremap i$ :<C-u> normal! T$vt$<CR>]]
+vim.cmd[[onoremap i$ :normal vi$<CR>]]
+vim.cmd[[xnoremap a$ :<C-u> normal!F$vf$<CR>]]
+vim.cmd[[onoremap a$ :normal va$<CR>]]
 
+-- deoplete auto completion for latex 
 function map(mode, lhs, rhs, opts)
 local options = { noremap = true }
 if opts then
@@ -11,25 +14,14 @@ end
 vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 -- can remove and copy lines adding a leader in order to paste with ctrl v 
--- map("n", "<leader>yy",' "+yy')
--- map("n", "<leader>dd",' "+dd')
--- map("i","<C-v>", "<C-r>+")
-
+map("n", "<leader>y",  "\"+y") map("v", "<leader>y",  "\"+y") map("n", "<leader>y",  "\"+Y") map("i","<C-v>", "<C-r>+")
 
 map("n","<leader>f", ":MaximizerToggle!<CR>")
 
-
-
-
+map("n", "<a-S-e>", "x $ p")
 vim.g.tmux_navigator_no_mappings = 1
-vim.cmd[[noremap <silent> <leader>h :w<CR>\|:<C-U>TmuxNavigateLeft<cr>]]
-vim.cmd[[noremap <silent> <leader>j :w<CR>\|:<C-U>TmuxNavigateDown<cr>]]
-vim.cmd[[noremap <silent> <leader>k :w<CR>\|:<C-U>TmuxNavigateUp<cr>]]
-vim.cmd[[noremap <silent> <leader>l :w<CR>\|:<C-U>TmuxNavigateRight<cr>]]
-vim.cmd[[noremap <silent> <leader>, <CR>\|:<C-U>TmuxNavigatePrevious<cr>]]
 
-
-
+map("i","jk", "<Esc>")
 map("n", "<C-j>", "10j")
 map("n", "<C-h>", "10h")
 map("n", "<C-l>", "10l")
@@ -39,7 +31,26 @@ map("n", "<C-k>", "10k")
 vim.keymap.set('n', '<leader>tp', function() 
     builtin.grep_string({search = vim.fn.input("Grep > ")});
 end
-) 
+)
+vim.keymap.set("n", "G", "G$")
+vim.keymap.set("v", "G", "G$")
+vim.keymap.set("n", "gg", "gg0")
+vim.keymap.set("v", "gg", "gg0")
+vim.keymap.set("n", ",", "$")
+vim.keymap.set("n", "d,", "d$")
+vim.keymap.set("n", "c,", "c$")
+vim.keymap.set("n", "c,", "c$")
+vim.keymap.set("n", "y,", "y$")
+vim.keymap.set("x", ",", "$")
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
+vim.g.tmux_resizer_no_mappings = 1
 
+vim.keymap.set("n", "²h", ":TmuxResizeLeft<CR>")
+vim.keymap.set("n", "²l", ":TmuxResizeRigh<CR>")
+vim.keymap.set("n", "²k", ":TmuxResizeUp<CR>")
+vim.keymap.set("n", "²j", ":TmuxResizeDown<CR>")
+--  x $pp
+
+vim.keymap.set("n", "<F6>", ":vs ~/.local/share/nvim/site/pack/packer/start/vim-snippets/snippets/ <CR>")
 
