f76834b3de03cdf9bca08b32852d1f14113ce905
bastien-mva <bastien.batardiere@gmail.com>
2022-12-26 11:22:17 +0100

    Changes for tmux and luasnip. keymaps.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,12 +1,26 @@
 local ls = require("luasnip")
 
-vim.keymap.set({ "i", "s" }, "<C-l>", function()
-          if ls.jumpable(1) then
-                            ls.jump(1)
+vim.keymap.set({ "i", "s" }, "<a-j>", function()
+          if ls.jumpable(-1) then
+                            ls.jump(-1)
                                             end
                                                         end, { silent = true })
 
 
+vim.keymap.set({ "i", "s" }, "<a-k>", function()
+          if ls.expand_or_jumpable() then
+                            ls.expand_or_jump()
+                                            end
+                                                        end, { silent = true })
 
+vim.keymap.set("n", "<leader><leader>s", "<cmd> source ~/.config/nvim/after/plugin/luasnip.lua <CR>")
 
+ls.snippets = {
+    -- Available in any filetype 
+    all = {ls.parser.parse_snippet("expand", "-- this is what was expanded!"),
+}, 
+lua = {
+    -- Lua specific snippets go here
+},  
+}
 
--- after/plugin/tmux.lua
+++ after/plugin/tmux.lua
@@ -1,18 +1,3 @@
-function map(mode, lhs, rhs, opts)
-local options = { noremap = true }
-if opts then
-options = vim.tbl_extend("force", options, opts)
-end
-vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-vim.g.mapleader = " " 
-map("n","<leader>f", ":MaximizerToggle!<CR>")
-
-
-
-
-vim.g.tmux_navigator_no_mappings = 1
 vim.cmd[[noremap <silent> <leader>h :w<CR>\|:<C-U>TmuxNavigateLeft<cr>]]
 vim.cmd[[noremap <silent> <leader>j :w<CR>\|:<C-U>TmuxNavigateDown<cr>]]
 vim.cmd[[noremap <silent> <leader>k :w<CR>\|:<C-U>TmuxNavigateUp<cr>]]
