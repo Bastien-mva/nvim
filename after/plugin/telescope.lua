9729054a9c5b7da35e406a2eff6782e1e421228a
bastien-mva <bastien.batardiere@gmail.com>
2023-01-03 23:00:45 +0100

    Added keymaps for live_grep and other stuff.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tt', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 
