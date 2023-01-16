a616ab849a1f0a3889a77dfc2e66bbe1905a1d2e
bastien-mva <bastien.batardiere@gmail.com>
2023-01-16 09:14:54 +0100

    Added shortcut for sh execution.

--- after/plugin/execFiles.lua
+++ after/plugin/execFiles.lua
@@ -3,4 +3,6 @@ vim.cmd[[autocmd FileType r nnoremap <buffer> <F8> :w \| Dispatch r  % <CR>]]
 vim.cmd[[autocmd FileType tex nnoremap <buffer> <F8> :w  <CR> :VimtexView <CR> ]]
 vim.cmd[[autocmd FileType html nnoremap <buffer> <F8> :w  <CR>  ]]
 vim.cmd[[autocmd FileType css nnoremap <buffer> <F8> :w  <CR>  ]]
+vim.cmd[[autocmd FileType sh nnoremap <buffer> <F8> :w  \|sp \| hor resize 10 \| term ./% <CR>]]
+vim.cmd[[autocmd FileType sh nnoremap <buffer> <F9> :w  \|sp \| hor resize 10 \| term ./% ]]
 
