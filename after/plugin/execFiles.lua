a3e927107b4b58f8682d26574fe0d905e574b6a0
bastien-mva <bastien.batardiere@gmail.com>
2023-03-13 10:05:58 +0100

    Change treesiter for cpp config and execution of cpp files uisng F6,7,8

--- after/plugin/execFiles.lua
+++ after/plugin/execFiles.lua
@@ -10,4 +10,7 @@ vim.cmd[[autocmd FileType sh nnoremap <buffer> <F8> :w  \|sp \| hor resize 10 \|
 vim.cmd[[autocmd FileType sh nnoremap <buffer> <F9> :w  \|sp \| hor resize 10 \| term ./% ]]
 vim.cmd[[autocmd FileType rst nnoremap <buffer> <F8> :w  \|:!make -C .. html  <CR>]]
 vim.cmd[[autocmd FileType python nnoremap <buffer> <F9> :w  \|:!make -C ~/These/pyPLNmodels/docs html  <CR>]]
-
+-- vim.cmd[[autocmd FileType cpp nnoremap <buffer> <F8> :w \| sp \| hor resize 10 \| term  clang++ -I /home/bastien/miniconda3/pkgs/libstdcxx-devel_linux-64-11.3.0-h210ce93_19/x86_64-conda-linux-gnu/include/c++/11.3.0  -o %  \|  <CR>]]
+vim.cmd[[autocmd FileType cpp nnoremap <buffer> <F6> :w \| sp \| hor resize 10 \|term  clang++ -I /home/bastien/miniconda3/pkgs/libstdcxx-devel_linux-64-11.3.0-h210ce93_19/x86_64-conda-linux-gnu/include/c++/11.3.0  % <CR> ]]
+vim.cmd[[autocmd FileType cpp nnoremap <buffer> <F7> :w \| sp \| hor resize 10 \|term  clang++ -I /home/bastien/miniconda3/pkgs/libstdcxx-devel_linux-64-11.3.0-h210ce93_19/x86_64-conda-linux-gnu/include/c++/11.3.0  % <CR> ZZ <CR> ]]
+vim.cmd[[autocmd FileType cpp nnoremap <buffer> <F8> :sp \| hor resize 10 \| term ./a.out <CR>]]
