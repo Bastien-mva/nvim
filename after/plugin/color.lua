a134f908ee77dc15f8c1700d42ed7a85cfbb0c57
bastien-mva <bastien.batardiere@gmail.com>
2024-07-31 14:07:18 +0200

    add lineNumbering colors.

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,5 +1,13 @@
 vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_transparent = true 
+vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
 
+function LineNumberColors()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='white', bold=true })
+end
+
 vim.cmd("colorscheme tokyonight")
+LineNumberColors()
+
