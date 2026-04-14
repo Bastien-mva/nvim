f56f0ef7ec6641c0a5cb3e8927525d5f308ec448
Bastien Batardiere <bastien.batardiere@fr.nestle.com>
2026-04-14 16:58:33 +0200

    update the file with :e each time I change so that nvim will update the changes when copilot changes.

--- after/plugin/tmux.lua
+++ after/plugin/tmux.lua
@@ -4,7 +4,7 @@
 -- vim.cmd[[noremap <silent> <leader>l :w<CR>\|:<C-U>TmuxNavigateRight<cr>]]
 vim.cmd[[noremap <silent> <leader>, <CR>\|:<C-U>TmuxNavigatePrevious<cr>]]
 
-vim.api.nvim_set_keymap('n', '<leader>h', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateLeft', false)<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>j', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateDown', false)<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>k', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateUp', false)<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>l', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateRight', false)<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>h', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateLeft', false)<CR> :e <CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>j', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateDown', false)<CR> :e <CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>k', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateUp', false)<CR> :e <CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>l', ":lua if vim.bo.buftype =='' then vim.api.nvim_exec('w', false) end; vim.api.nvim_exec('TmuxNavigateRight', false)<CR> :e <CR>", {noremap = true, silent = true})
